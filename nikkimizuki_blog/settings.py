9b2e9920ee798b3480b4e03af3a436a9258f3966
jortompk <jortompk@gmail.com>
2025-01-08 23:54:24 -0500

    added blog

--- nikkimizuki_blog/settings.py
+++ nikkimizuki_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%1d44$(vus_wwsm9v(%u(4qyi5ddr@k*d+io)l4t&dpkyex4pe'
+SECRET_KEY = 'django-insecure-b&c1!w%zhp^@3-t(enqnc(*jag%^%ja)m6gau7mby3xs#@r%rl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
