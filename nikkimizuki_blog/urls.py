32284ae4d0dda4cfd73f18cafaa53fdf1feecd27
jortompk <jortompk@gmail.com>
2025-01-13 20:17:01 -0500

    updating blog models

--- nikkimizuki_blog/urls.py
+++ nikkimizuki_blog/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('gallery/', include("gallery.urls")),
+    #path('gallery/', include("gallery.urls")),
     path('blog/', include("blog.urls")),
     path('admin/', admin.site.urls),
 ]
